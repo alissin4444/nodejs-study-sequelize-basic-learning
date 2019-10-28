c8d112d620fb1435545cc4981a7a4313e81d8204
Alisson Santos <45664013+alissin4444@users.noreply.github.com>
2019-10-28 00:57:19 -0300

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: "postgres",
+    host: "salt.db.elephantsql.com",
+    username: "kqjjzobo",
+    password: "NBB5oGkjKU_XA-oYpPtjl-LCL4dG5nr-",
+    database: "kqjjzobo",
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+};
